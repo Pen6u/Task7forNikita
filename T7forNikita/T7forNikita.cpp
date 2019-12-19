54b6984fb18d5011045e082579bd8d31c7c59906
Смородин Даниил <Pen6u@users.noreply.github.com>
2019-12-19 22:40:35 +0300

    готов

--- T7forNikita/T7forNikita.cpp
+++ T7forNikita/T7forNikita.cpp
@@ -5,7 +5,7 @@
 
 std::string Input();
 bool IsSizeSame(std::string& str1, std::string& str2);
-bool IsPosible(std::string& str1, std::string& str2);
+bool IsPosible(std::string& str1, std::string& str2, bool sizeAnswer);
 void PrintAnswer(bool answer);
 
 int main() {
@@ -13,7 +13,8 @@ int main() {
 	SetConsoleOutputCP(1251);
 	std::string str1 = Input();
 	std::string str2 = Input();
-	bool answer = IsSizeSame(str1, str2);
+	bool sizeAnswer = IsSizeSame(str1, str2);
+	bool answer = IsPosible(str1, str2, sizeAnswer);
 	PrintAnswer(answer);
 	system("PAUSE");
 	return 0;
@@ -31,35 +32,37 @@ bool IsSizeSame(std::string& str1, std::string& str2) {
 	int size2 = str2.size();
 	if ( size1 == size2 ) {
 		return 1;
-	} else {
-		return 0;
 	}
 }
 
-bool IsPosible(std::string& str1, std::string& str2) {
-	if ( IsSizeSame ) {
-		int size = str1.size();
-		//int counter = 0;
-		for ( int i = 0; i < size; i++ ) {
-			for ( int j = 0; j < size; j++ )
-				if ( str1[i] == str2[j] ) {
+bool IsPosible(std::string& str1, std::string& str2, bool sizeAnswer) {
+	if (sizeAnswer) {
+		short size1 = str1.size();
+		short size2 = str2.size();
+		short count = size1;
+		for (int i = 0; i < size1; i++) {
+			for (int j = 0; j < size2; j++)
+				if (str1[i] == str2[j]) {
 					str2.erase(j, 1);
-					//counter++;
-				} else {
+					size2--;
+					count--;
+					if ( count == 0 ) {
+						return 1;
+					}
+				}
+				else {
 					return 0;
 				}
 		}
 		return 1;
-	} else {
-		return 0;
 	}
 }
 
 void PrintAnswer(bool answer) {
 	if ( answer ) {
-		std::cout << "Да, можно приобразовать" << std::endl;
+		std::cout << "Да, можно преобразовать" << std::endl;
 	}
 	else {
-		std::cout << "Нет, нельзя приобразовать" << std::endl;
+		std::cout << "Нет, нельзя преобразовать" << std::endl;
 	}
 }
